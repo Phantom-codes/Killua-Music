203643999d1663a834895f8adc819f898e989404
Phantom <73062410+Phantom-codes@users.noreply.github.com>
2021-07-09 12:09:17 +0530

    Removed db

--- commands/Bot Info/prefix.js
+++ commands/Bot Info/prefix.js
@@ -1,4 +1,3 @@
-const db = require('quick.db')
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
@@ -12,14 +11,10 @@ module.exports = {
             message.guild.members.cache.get(args[0]) ||
             message.guild.member(message.author);
 
-        gprefix = db.get(`gprefix-${message.guild.id}`);
-        if (!gprefix) {
-            gprefix = 'Not Setted Use `kl!setprefix` to set one.'
-        }
         p1 = '`kl!`';
         p2 = '`Killua` or `@Killua#4419`';
         const embed = new MessageEmbed()
-            .setDescription(`Hello,\nMy Default prefix are ${p1} and ${p2}\nThe customised guild prefix is ${gprefix}\nUse `+'`kl!play [song]`'+` and lets start vibing`)
+            .setDescription(`Hello,\nMy Default prefix are ${p1} and ${p2}\nUse `+'`kl!play [song]`'+` and lets start vibing`)
             .setColor(userinfoget.displayHexColor)
             .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
         message.channel.send(embed)
